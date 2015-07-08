f79f5905226fe426785f95f74d6e4b5c653e611b
Trent Oswald <trentoswald@therebelrobot.com>
2015-07-08 16:33:26 -0600

    Still working out multiples

--- test/diff-compare.test.js
+++ test/diff-compare.test.js
@@ -5,7 +5,7 @@ debug.enable()
 /* Definitions for JS Standard */
 /* global describe, it */
 
-describe('diff', function () {
+describe('diff-compare', function () {
   it('should adjust for simple omissions', function (done) {
     var a = ['abc','def']
     var b = ['def']
@@ -164,16 +164,182 @@ describe('diff', function () {
     expect(adjusted.annotations[1].inert).to.deep.equal([])
     done()
   })
-  it('should adjust for multi-line omissions with an end change', function (done) {
+  it('should adjust for multiple simple additions', function (done) {
+
+    var a = ['def','jkl']
+    var b = ['abc','def','ghi','jkl']
+
+    var a_adjusted = ['','def','','jkl']
+    var b_adjusted = ['abc','def','ghi','jkl']
+
+    var adjusted = diff(a,b)
+    expect(adjusted).to.be.an('object')
+    expect(adjusted.results).to.be.an('array')
+    expect(adjusted.results[0]).to.be.an('array')
+    expect(adjusted.results[0]).to.deep.equal(a_adjusted)
+    expect(adjusted.results[1]).to.be.an('array')
+    expect(adjusted.results[1]).to.deep.equal(b_adjusted)
+    expect(adjusted.annotations).to.be.an('array')
+    expect(adjusted.annotations[0]).to.be.an('object')
+    expect(adjusted.annotations[0].omissions).to.be.an('array')
+    expect(adjusted.annotations[0].omissions).to.deep.equal([])
+    expect(adjusted.annotations[0].changes).to.be.an('array')
+    expect(adjusted.annotations[0].changes).to.deep.equal([])
+    expect(adjusted.annotations[0].inert).to.be.an('array')
+    expect(adjusted.annotations[0].inert).to.deep.equal([0,2])
+    expect(adjusted.annotations[1]).to.be.an('object')
+    expect(adjusted.annotations[1].additions).to.be.an('array')
+    expect(adjusted.annotations[1].additions).to.deep.equal([0,2])
+    expect(adjusted.annotations[1].changes).to.be.an('array')
+    expect(adjusted.annotations[1].changes).to.deep.equal([])
+    expect(adjusted.annotations[1].inert).to.be.an('array')
+    expect(adjusted.annotations[1].inert).to.deep.equal([])
+    done()
   })
-  it('should adjust for multi-line additions with an end change', function (done) {
+  it('should adjust for multiple simple ommissions', function (done) {
+
+    var b = ['def','jkl']
+    var a = ['abc','def','ghi','jkl']
+
+    var b_adjusted = ['','def','','jkl']
+    var a_adjusted = ['abc','def','ghi','jkl']
+
+    var adjusted = diff(a,b)
+    expect(adjusted).to.be.an('object')
+    expect(adjusted.results).to.be.an('array')
+    expect(adjusted.results[0]).to.be.an('array')
+    expect(adjusted.results[0]).to.deep.equal(a_adjusted)
+    expect(adjusted.results[1]).to.be.an('array')
+    expect(adjusted.results[1]).to.deep.equal(b_adjusted)
+    expect(adjusted.annotations).to.be.an('array')
+    expect(adjusted.annotations[0]).to.be.an('object')
+    expect(adjusted.annotations[0].omissions).to.be.an('array')
+    expect(adjusted.annotations[0].omissions).to.deep.equal([0,2])
+    expect(adjusted.annotations[0].changes).to.be.an('array')
+    expect(adjusted.annotations[0].changes).to.deep.equal([])
+    expect(adjusted.annotations[0].inert).to.be.an('array')
+    expect(adjusted.annotations[0].inert).to.deep.equal([])
+    expect(adjusted.annotations[1]).to.be.an('object')
+    expect(adjusted.annotations[1].additions).to.be.an('array')
+    expect(adjusted.annotations[1].additions).to.deep.equal([])
+    expect(adjusted.annotations[1].changes).to.be.an('array')
+    expect(adjusted.annotations[1].changes).to.deep.equal([])
+    expect(adjusted.annotations[1].inert).to.be.an('array')
+    expect(adjusted.annotations[1].inert).to.deep.equal([0,2])
+    done()
   })
-  it('should adjust for multi-line omissions with a simple addition', function (done) {
+  it('should adjust for multiple simple changes', function (done) {
+
+    var a = ['abc','def','ggi','jkl']
+    var b = ['acc','def','ghi','jkl']
+
+    var a_adjusted = ['abc','def','ggi','jkl']
+    var b_adjusted = ['acc','def','ghi','jkl']
+
+    var adjusted = diff(a,b)
+    expect(adjusted).to.be.an('object')
+    expect(adjusted.results).to.be.an('array')
+    expect(adjusted.results[0]).to.be.an('array')
+    expect(adjusted.results[0]).to.deep.equal(a_adjusted)
+    expect(adjusted.results[1]).to.be.an('array')
+    expect(adjusted.results[1]).to.deep.equal(b_adjusted)
+    expect(adjusted.annotations).to.be.an('array')
+    expect(adjusted.annotations[0]).to.be.an('object')
+    expect(adjusted.annotations[0].omissions).to.be.an('array')
+    expect(adjusted.annotations[0].omissions).to.deep.equal([])
+    expect(adjusted.annotations[0].changes).to.be.an('array')
+    expect(adjusted.annotations[0].changes).to.deep.equal([0,2])
+    expect(adjusted.annotations[0].inert).to.be.an('array')
+    expect(adjusted.annotations[0].inert).to.deep.equal([])
+    expect(adjusted.annotations[1]).to.be.an('object')
+    expect(adjusted.annotations[1].additions).to.be.an('array')
+    expect(adjusted.annotations[1].additions).to.deep.equal([])
+    expect(adjusted.annotations[1].changes).to.be.an('array')
+    expect(adjusted.annotations[1].changes).to.deep.equal([0,2])
+    expect(adjusted.annotations[1].inert).to.be.an('array')
+    expect(adjusted.annotations[1].inert).to.deep.equal([])
+    done()
   })
-  it('should adjust for multi-line additions with a simple omission', function (done) {
+  it('should adjust for multiple simple additions + simple ommissions', function (done) {
+
+    var a = ['def','jkl','mno','pqr','stu','vwx']
+    var b = ['abc','def','ghi','jkl','pqr','vwx']
+
+    var a_adjusted = ['','def','','jkl','mno','pqr','stu','vwx']
+    var b_adjusted = ['abc','def','ghi','jkl','','pqr','','vwx']
+
+    var adjusted = diff(a,b)
+    expect(adjusted).to.be.an('object')
+    expect(adjusted.results).to.be.an('array')
+    expect(adjusted.results[0]).to.be.an('array')
+    expect(adjusted.results[0]).to.deep.equal(a_adjusted)
+    expect(adjusted.results[1]).to.be.an('array')
+    expect(adjusted.results[1]).to.deep.equal(b_adjusted)
+    expect(adjusted.annotations).to.be.an('array')
+    expect(adjusted.annotations[0]).to.be.an('object')
+    expect(adjusted.annotations[0].omissions).to.be.an('array')
+    expect(adjusted.annotations[0].omissions).to.deep.equal([4,6])
+    expect(adjusted.annotations[0].changes).to.be.an('array')
+    expect(adjusted.annotations[0].changes).to.deep.equal([])
+    expect(adjusted.annotations[0].inert).to.be.an('array')
+    expect(adjusted.annotations[0].inert).to.deep.equal([0,2])
+    expect(adjusted.annotations[1]).to.be.an('object')
+    expect(adjusted.annotations[1].additions).to.be.an('array')
+    expect(adjusted.annotations[1].additions).to.deep.equal([0,2])
+    expect(adjusted.annotations[1].changes).to.be.an('array')
+    expect(adjusted.annotations[1].changes).to.deep.equal([])
+    expect(adjusted.annotations[1].inert).to.be.an('array')
+    expect(adjusted.annotations[1].inert).to.deep.equal([4,6])
+    done()
   })
-  it('should adjust for multi-line omissions with multi-line additions', function (done) {
+  it('should adjust for multiple simple omissions + simple additions', function (done) {
+
+    var b = ['def','jkl','mno','pqr','stu','vwx']
+    var a = ['abc','def','ghi','jkl','pqr','vwx']
+
+    var b_adjusted = ['','def','','jkl','mno','pqr','stu','vwx']
+    var a_adjusted = ['abc','def','ghi','jkl','','pqr','','vwx']
+
+    var adjusted = diff(a,b)
+    expect(adjusted).to.be.an('object')
+    expect(adjusted.results).to.be.an('array')
+    expect(adjusted.results[0]).to.be.an('array')
+    expect(adjusted.results[0]).to.deep.equal(a_adjusted)
+    expect(adjusted.results[1]).to.be.an('array')
+    expect(adjusted.results[1]).to.deep.equal(b_adjusted)
+    expect(adjusted.annotations).to.be.an('array')
+    expect(adjusted.annotations[0]).to.be.an('object')
+    expect(adjusted.annotations[0].omissions).to.be.an('array')
+    expect(adjusted.annotations[0].omissions).to.deep.equal([0,2])
+    expect(adjusted.annotations[0].changes).to.be.an('array')
+    expect(adjusted.annotations[0].changes).to.deep.equal([])
+    expect(adjusted.annotations[0].inert).to.be.an('array')
+    expect(adjusted.annotations[0].inert).to.deep.equal([4,6])
+    expect(adjusted.annotations[1]).to.be.an('object')
+    expect(adjusted.annotations[1].additions).to.be.an('array')
+    expect(adjusted.annotations[1].additions).to.deep.equal([4,6])
+    expect(adjusted.annotations[1].changes).to.be.an('array')
+    expect(adjusted.annotations[1].changes).to.deep.equal([])
+    expect(adjusted.annotations[1].inert).to.be.an('array')
+    expect(adjusted.annotations[1].inert).to.deep.equal([0,2])
+    done()
   })
-  it('should adjust for multi-line additions with multi-line omissions', function (done) {
-  })
+  // it('should adjust for multiple simple additions + simple changes', function (done) {
+  // })
+  // it('should adjust for multiple simple ommissions + simple changes', function (done) {
+  // })
+  // it('should adjust for multiple multi-line additions', function (done) {
+  // })
+  // it('should adjust for multiple multi-line ommissions', function (done) {
+  // })
+  // it('should adjust for multiple multi-line additions + multi-line ommissions', function (done) {
+  // })
+  // it('should adjust for multi-line additions with an end change', function (done) {
+  // })
+  // it('should adjust for multi-line omissions with a simple addition', function (done) {
+  // })
+  // it('should adjust for multi-line additions with a simple omission', function (done) {
+  // })
+  // it('should adjust for eof addition/omission/changes', function (done) {
+  // })
 })
